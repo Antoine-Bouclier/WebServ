187d2792e244e14c345e3b01211606492717fab8
Antoine-Bouclier <antoinebouclier1@gmail.com>
2026-02-19 18:06:23 +0100

    Adding TokenType and Token

--- inc/WebServ.hpp
+++ inc/WebServ.hpp
@@ -6,4 +6,19 @@
 
 #include <sys/types.h>
 
+enum TokenType
+{
+	TOKEN_WORD,
+	TOKEN_LBRACE,
+	TOKEN_RBRACE,
+	TOKEN_SEMICOLON,
+	TOKEN_EOF
+};
+
+struct Token
+{
+	TokenType	type;
+	std::string	value;
+}
+
 #endif
